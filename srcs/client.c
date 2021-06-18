94b66146fd74dc74b335665ef9aabf70110c9efd
hugsbord <hugsbord@student.42.fr>
2021-06-18 22:13:40 +0200

    bin/dec converters

--- srcs/client.c
+++ srcs/client.c
@@ -6,16 +6,66 @@
 /*   By: hugsbord <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 14:40:11 by hugsbord          #+#    #+#             */
-/*   Updated: 2021/06/17 09:46:27 by hugsbord         ###   ########.fr       */
+/*   Updated: 2021/06/18 20:36:19 by hugsbord         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../includes/minitalk.h"
 
+void	send_message(char *msg)
+{
+
+}
+void	char_to_bin(int c)
+{
+	int		bin;
+	int		i;
+	int		j;
+	int		tab[10];
+
+	bin = 0;
+	i = 0;
+	while (c > 0)
+	{
+		tab[i] = c % 2;
+		c = c / 2;
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		ft_putnbr_fd(tab[i], 1);
+		i--;
+	}
+	ft_putstr_fd("\n", 1);
+}
+
+void	signal_handler(int num)
+{
+	
+}
+
 int		main(int argc, char **argv)
 {
-	ft_putstr_fd("test", 1);
-	if (argc != 2)
-		return (1);
+	int		pid;
+	char	*msg;
+	int i = 0;
+
+	if (argc != 3)
+	{
+		ft_putstr_fd("Usage: ./client [PID] [message]\n", 1);
+		exit (0);
+	}
+	
+	signal(SIGUSR1, signal_handler);
+	while (argv[2][i])
+	{
+		ft_putchar_fd(argv[2][i],1);
+//		ft_putnbr_fd(argv[2][i],1);
+		char_to_bin(argv[2][i]);
+		i++;
+	}
+	pid = ft_atoi(argv[1]);
+	signal(SIGUSR2, signal_handler);
 	return (0);
 }
--- srcs/server.c
+++ srcs/server.c
@@ -6,18 +6,48 @@
 /*   By: hugsbord <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 14:28:56 by hugsbord          #+#    #+#             */
-/*   Updated: 2021/06/17 09:45:18 by hugsbord         ###   ########.fr       */
+/*   Updated: 2021/06/18 22:11:10 by hugsbord         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../includes/minitalk.h"
 
+int		bin_to_dec(int bin)
+{
+	int		num;
+	int		dec;
+	int		base;
+	int		rem;
+
+	num = bin;
+	dec = 0;
+	base = 1;
+	rem = 0;
+	while (num > 0)
+	{
+		rem = num % 10;
+		dec = dec + rem * base; 
+		num = num / 10;
+		base = base * 2;
+	}
+	ft_putnbr_fd(dec, 1);
+	return (dec);
+}
+
+void	signal_handler(int num)
+{
+	
+}
+
 int		main(void)
 {
 	int		pid;
-	char	*display_pid;
+	char	*show_pid;
 
 	pid = getpid();
-	display_pid = ft_itoa(pid);
+	show_pid = ft_itoa(pid);
+	ft_putstr_fd(show_pid, 1);
+	ft_putchar_fd('\n', 1);
+	bin_to_dec(1110011);
 	return (0);
 }
